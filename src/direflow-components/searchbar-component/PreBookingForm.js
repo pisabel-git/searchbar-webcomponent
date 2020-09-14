36cb1c3b8d9a6f8af892ce8c1fa55f0429465986
Pierre Isabel <pierre.isabel@octo.com>
2020-09-14 17:28:53 +0200

    PrebookingForm variables

--- src/direflow-components/searchbar-component/PreBookingForm.js
+++ src/direflow-components/searchbar-component/PreBookingForm.js
@@ -1,12 +1,15 @@
-import React, {useState} from "react"
+import React, { useState } from "react"
 
 export const PreBookingFormContext = React.createContext()
 
-export default function PreBookingForm({children}) {
+export default function PreBookingForm({ children }) {
 
     const [nbAdults, setNbAdults] = useState(0)
     const [nbChildren, setNbChildren] = useState(0)
     const [childrenList, setChildrenList] = useState([])
+    const [startDate, setStartDate] = useState(null)
+    const [endDate, setEndDate] = useState(null)
+    const [focusedInput, setFocusedInput] = useState(null)
 
     const bookingContext = {
         nbAdults: nbAdults,
@@ -14,9 +17,15 @@ export default function PreBookingForm({children}) {
         nbChildren: nbChildren,
         setNbChildren: (val) => setNbChildren(val),
         childrenList: childrenList,
-        setChildrenList: (val) => setChildrenList(val)
+        setChildrenList: (val) => setChildrenList(val),
+        startDate: startDate,
+        setStartDate: (val) => setStartDate(val),
+        endDate: endDate,
+        setEndDate: (val) => setEndDate(val),
+        focusedInput: focusedInput,
+        setFocusedInput: (val) => setFocusedInput(val)
     }
-    
+
     return (
         <PreBookingFormContext.Provider value={bookingContext}>
             {children}
